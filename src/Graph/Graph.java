aee52b93158bf684cc5a79565d9ace04524cf40b
mxzpytlk <mxzpytlk@gmail.com>
2020-09-27 23:08:41 +0300

    Add graph to project

---
+++ src/Graph/Graph.java
@@ -0,0 +1,131 @@
+package Graph;
+
+import javafx.util.Pair;
+
+import java.util.*;
+
+public class Graph<T> implements Iterable<T> {
+    private List<List<Pair<Integer, T>>> vEdjLists = new ArrayList<>();
+    private int eCount = 0;
+
+    public Graph(){ }
+
+    public int vertexCount() {
+        return vEdjLists.size();
+    }
+
+    public int edgeCount() {
+        return eCount;
+    }
+
+    public void addEdge(T v1, T v2) {
+        int index1 = indexOfVertex(v1);
+        int index2 = indexOfVertex(v2);
+        if (index1 == -1) {
+            vEdjLists.add(new LinkedList<>());
+            vEdjLists.get(vEdjLists.size() - 1).add(new Pair<>(vEdjLists.size() - 1, v1));
+            index1 = vEdjLists.size() - 1;
+        }
+
+        if (index2 == -1) {
+            vEdjLists.add(new LinkedList<>());
+            vEdjLists.get(vEdjLists.size() - 1).add(new Pair<>(vEdjLists.size() - 1, v2));
+            index2 = vEdjLists.size() - 1;
+        }
+
+        if (!isEdj(v1, v2)) {
+            vEdjLists.get(index1).add(vEdjLists.get(index2).get(0));
+            eCount++;
+        }
+    }
+
+    public boolean isEdj(T v1, T v2) {
+        if (v1 == v2) return true;
+        for (T adj : edjacencies(v1)) {
+            if (adj.equals(v2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void addVertex(T v) {
+        if (indexOfVertex(v) == -1) {
+            vEdjLists.add(new LinkedList<>());
+            vEdjLists.get(vEdjLists.size() - 1).add(new Pair<>(vEdjLists.size() - 1, v));
+        }
+    }
+
+    public void removeEdge(T v1, T v2) {
+        int index1 = indexOfVertex(v1);
+        int index2 = indexOfVertex(v2);
+        if (index1 == -1 || index2 == -1 || !vEdjLists.get(index1).contains(vEdjLists.get(index2).get(0))) return;
+        vEdjLists.get(index1).remove(vEdjLists.get(index2).get(0));
+        eCount--;
+    }
+
+    private int indexOfVertex(T v1) {
+        for (List<Pair<Integer, T>> l : vEdjLists)
+            if (l.get(0).getValue().equals(v1)) return l.get(0).getKey();
+        return -1;
+    }
+
+
+    public Iterable<T> edjacencies(T v) {
+        Iterator<Pair<Integer, T>> it = vEdjLists.get(indexOfVertex(v)).iterator();
+        it.next();
+        return () -> new Iterator<>() {
+
+            @Override
+            public boolean hasNext() {
+                return it.hasNext();
+            }
+
+            @Override
+            public T next() {
+                return it.next().getValue();
+            }
+        };
+    }
+
+    public Iterable<T> bfs(T from) {
+        return new Iterable<>() {
+            private Queue<T> queue = null;
+            private boolean[] visited = null;
+
+            @Override
+            public Iterator<T> iterator() {
+                queue = new LinkedList<>();
+                queue.add(from);
+                visited = new boolean[Graph.this.vertexCount()];
+                visited[indexOfVertex(from)] = true;
+
+                return new Iterator<>() {
+                    @Override
+                    public boolean hasNext() {
+                        return !queue.isEmpty();
+                    }
+
+                    @Override
+                    public T next() {
+                        T result = queue.remove();
+                        for (T adj : Graph.this.edjacencies(result)) {
+                            int index = indexOfVertex(adj);
+                            if (!visited[index]) {
+                                visited[index] = true;
+                                queue.add(adj);
+                            }
+                        }
+                        return result;
+                    }
+                };
+            }
+        };
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return bfs(vEdjLists.get(0).get(0).getValue()).iterator();
+    }
+}
---
+++ src/vsu/course2/Field.java
@@ -0,0 +1,4 @@
+package vsu.course2;
+
+public class Field {
+}
