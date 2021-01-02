11bd873bb2900acc0503c5a2307e128fe0da0652
mxzpytlk <mxzpytlk@gmail.com>
2021-01-02 14:56:16 +0300

    Добавлена библиотека GSON в проект

--- src/main/java/vsu/course2/Main.java
+++ src/main/java/vsu/course2/Main.java
@@ -1,11 +1,13 @@
 package vsu.course2;
 
+import com.google.gson.Gson;
 import vsu.course2.models.game.Game;
 import vsu.course2.services.ConsoleInterfaceService;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    new ConsoleInterfaceService().startGame(new Game());
+    public static void main(String[] args) { ;
+        new ConsoleInterfaceService().startGame(new Game());
+
     }
 }
